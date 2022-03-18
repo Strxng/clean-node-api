13d8a669352fa602b48b2aac64c1cbbd5b4fd9fd
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-03-18 13:25:17 -0300

    Move all sut creations for a method

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,22 +1,26 @@
 const LoginRouter = require('./loginRouter')
 const MissingParamError = require('../helpers/missingParamError')
 
+const makeSut = () => {
+  return new LoginRouter()
+}
+
 // Testes
 describe('Login router', () => {
   it('Should return 500 if no httpRequest is provided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
   it('Should return 500 if no httpRequest has no body', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpResponse = sut.route({})
     expect(httpResponse.statusCode).toBe(500)
   })
 
   it('Should return 400 if no email is provided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         password: 'any_pass'
@@ -28,7 +32,7 @@ describe('Login router', () => {
   })
 
   it('Should return 400 if no password is provided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com'
