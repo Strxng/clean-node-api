424bd3eb060b9c939b7da47d1b0f17dbdd096824
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-03-22 15:43:51 -0300

    jest ignore history

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['**/src/**/*js'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
+  modulePathIgnorePatterns: ['.history']
 }
