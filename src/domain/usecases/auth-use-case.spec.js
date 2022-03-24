b8082cccbe3cdf78dc64e879c253a3f35cf3d046
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-03-24 17:55:43 -0300

    Refactor authUseCase for a new file

--- src/domain/usecases/auth-use-case.spec.js
+++ src/domain/usecases/auth-use-case.spec.js
@@ -1,24 +1,5 @@
 const { MissingParamError } = require('../../utils/errors')
-
-class AuthUseCase {
-  constructor (loadUserByEmailRepository) {
-    this.loadUserByEmailRepository = loadUserByEmailRepository
-  }
-
-  async auth (email, password) {
-    if (!email) {
-      throw new MissingParamError('email')
-    }
-    if (!password) {
-      throw new MissingParamError('password')
-    }
-
-    const user = await this.loadUserByEmailRepository.load(email)
-    if (!user) {
-      return null
-    }
-  }
-}
+const AuthUseCase = require('./authUseCase')
 
 const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
