dff90f29c9af846c1f92b509df0d6299fd840997
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-03-25 09:50:25 -0300

    Ensure authUseCase returns an accessToken if valid credentials are provided

--- src/domain/usecases/auth-use-case.spec.js
+++ src/domain/usecases/auth-use-case.spec.js
@@ -112,4 +112,11 @@ describe('authUseCase', () => {
     await sut.auth('valid_email@email.com', 'valid_password')
     expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
   })
+
+  it('Should return an accessToken if correct credentials are provided', async () => {
+    const { sut, tokenGeneratorSpy } = makeSut()
+    const accessToken = await sut.auth('valid_email@email.com', 'valid_password')
+    expect(accessToken).toBe(tokenGeneratorSpy.accessToken)
+    expect(accessToken).toBeTruthy()
+  })
 })
