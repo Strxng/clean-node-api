4d86cac6e82db21b863e72a25edce26fa72829e1
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-03-25 09:05:05 -0300

    Ensure authUseCase returns null if an invalid pass or email is provided

--- src/domain/usecases/auth-use-case.spec.js
+++ src/domain/usecases/auth-use-case.spec.js
@@ -5,10 +5,12 @@ const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
     async load (email) {
       this.email = email
+      return this.user
     }
   }
 
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  loadUserByEmailRepositorySpy.user = {}
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
   return {
     loadUserByEmailRepositorySpy,
@@ -47,9 +49,16 @@ describe('authUseCase', () => {
     await expect(promise).rejects.toThrow()
   })
 
-  it('Should return null if loadUserByEmail returns null', async () => {
+  it('Should return null if an invalid email is provided', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    loadUserByEmailRepositorySpy.user = null
+    const accessToken = await sut.auth('invalid_email@email.com', 'valid_password')
+    expect(accessToken).toBe(null)
+  })
+
+  it('Should return null if an invalid password is provided', async () => {
     const { sut } = makeSut()
-    const accessToken = await sut.auth('invalid_email@email.com', 'any_email')
+    const accessToken = await sut.auth('valid_email@email.com', 'invalid_password')
     expect(accessToken).toBe(null)
   })
 })
