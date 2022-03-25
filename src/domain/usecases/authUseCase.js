8f512b308732572665702d2231d7a420aa044693
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-03-25 10:06:07 -0300

    refactor to simplify auth method

--- src/domain/usecases/authUseCase.js
+++ src/domain/usecases/authUseCase.js
@@ -16,11 +16,7 @@ module.exports = class AuthUseCase {
     }
 
     const user = await this.loadUserByEmailRepository.load(email)
-    if (!user) {
-      return null
-    }
-
-    const isValid = await this.encrypter.compare(password, user.password)
+    const isValid = user && await this.encrypter.compare(password, user.password)
     if (!isValid) {
       return null
     }
