35352f5e74d16b1508c10383c7bc8668d369472b
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-04-25 17:23:31 -0300

    add tokenSecret in env file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxxx/clean-node-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxxx/clean-node-api',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
