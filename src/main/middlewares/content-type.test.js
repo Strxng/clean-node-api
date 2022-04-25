4b585b478b8356749aacfab01e2c7566c510a53c
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-04-25 15:17:35 -0300

    Reset jest modules in content-type middleware before all tests

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest')
-const app = require('../config/app')
 
 describe('Content-type Middleware', () => {
+  let app
+
+  beforeEach(() => {
+    jest.resetModules()
+    app = require('../config/app')
+  })
+
   it('Should return json content-type as default', async () => {
     app.get('/test_content_type', (req, res) => {
       res.send('')
@@ -11,11 +17,11 @@ describe('Content-type Middleware', () => {
   })
 
   it('Should return xml content-type is forced', async () => {
-    app.get('/test_content_type_xml', (req, res) => {
+    app.get('/test_content_type', (req, res) => {
       res.type('xml')
       res.send('')
     })
 
-    await request(app).get('/test_content_type_xml').expect('content-type', /xml/)
+    await request(app).get('/test_content_type').expect('content-type', /xml/)
   })
 })
