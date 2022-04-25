93e69237b81434670d06f256da564e37e148ea8b
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-04-25 14:14:19 -0300

    Enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -10,4 +10,15 @@ describe('App setup', () => {
     const response = await request(app).get('/test_x_powered_by')
     expect(response.headers['x-powered-by']).toBe(undefined)
   })
+
+  it('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+
+    const response = await request(app).get('/test_cors')
+    expect(response.headers['access-control-allow-origin']).toBe('*')
+    expect(response.headers['access-control-allow-methods']).toBe('*')
+    expect(response.headers['access-control-allow-headers']).toBe('*')
+  })
 })
